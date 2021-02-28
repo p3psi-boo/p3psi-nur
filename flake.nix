890d2f0f0680aa445ff8f288143dada4590c7127
p3psi <carr0t-bu@outlook.com>
2021-03-01 00:53:00 +0800

    flake overlay test

--- flake.nix
+++ flake.nix
@@ -1,7 +1,12 @@
 {
   description = "P3psi's nur";
-  outputs = { self }: {
-    overlay = final: prev:
-      import ./overlay.nix final prev;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      linux_gru_4_4 = prev.callPackage ./pkgs/linux_gru_4_4;
+      stm8flash = prev.callPackage ./pkgs/stm8flash;
+    };
   };
 }
